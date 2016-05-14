2a5fc02633c618b049d4c68e91786603fe93bb92
Yuen-rMBP <rxg9527@sina.cn>
2016-05-14 09:49:09 +0800

    Initial Commit

---
+++ Objective-C_201605/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Objective-C_201605
+//
+//  Created by Yuen on 16/5/14.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Objective-C_201605/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Objective-C_201605
+//
+//  Created by Yuen on 16/5/14.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
