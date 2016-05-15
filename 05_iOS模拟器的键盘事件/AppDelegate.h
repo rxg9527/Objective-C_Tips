90002eba4fb4975724cf3660520b11664182906f
Yuen-rMBP <rxg9527@sina.cn>
2016-05-15 15:06:01 +0800

    nil

---
+++ 05_iOS模拟器的键盘事件/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  05_iOS模拟器的键盘事件
+//
+//  Created by Yuen on 16/5/15.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
