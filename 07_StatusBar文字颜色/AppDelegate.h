c3116c4a1d684adf51cb7b5ed8b7d9fe700e9aac
Yuen-rMBP <rxg9527@sina.cn>
2016-05-15 15:21:33 +0800

    nil

---
+++ 07_StatusBar文字颜色/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  07_StatusBar文字颜色
+//
+//  Created by Yuen on 16/5/15.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
