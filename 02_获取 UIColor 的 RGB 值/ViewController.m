72efe178cda0c318721e913e2708c1131baa90ec
Yuen-rMBP <rxg9527@sina.cn>
2016-05-15 11:58:44 +0800

    通过 CGColorGetComponents 获取 UIColor 的 RGB 值

--- 02_获取 UIColor 的 RGB 值/ViewController.m
+++ 02_获取 UIColor 的 RGB 值/ViewController.m
@@ -18,7 +18,19 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    
+    NSUInteger R = (arc4random() % 256) ;
+    NSUInteger G = (arc4random() % 256) ;
+    NSUInteger B = (arc4random() % 256) ;
+    UIColor *randomColor = [UIColor colorWithRed:R/255.0 green:G/255.0 blue:B/255.0 alpha:1];
+
+    /**
+     *  通过 CGColorGetComponents 获取 UIColor 的 RGB 值
+     */
+    const CGFloat *components = CGColorGetComponents(randomColor.CGColor);
+    NSLog(@"Red: %f", components[0]);
+    NSLog(@"Green: %f", components[1]);
+    NSLog(@"Blue: %f", components[2]);
+    NSLog(@"Alpha: %f", components[3]);
 }
 
 - (void)didReceiveMemoryWarning {
