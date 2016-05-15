da4724dd8a33d846d656e30c3a72b7fd5456bcff
Yuen-rMBP <rxg9527@sina.cn>
2016-05-15 12:00:40 +0800

    nil

---
+++ 03_计算两点之间的距离/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  03_计算两点之间的距离
+//
+//  Created by Yuen on 16/5/15.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
