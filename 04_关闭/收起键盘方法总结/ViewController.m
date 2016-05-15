5271c153a28d71829f064d4f41d9af5ab3dcd697
Yuen-rMBP <rxg9527@sina.cn>
2016-05-15 15:04:27 +0800

    关闭/收起键盘方法总结 1、点击Return按扭时收起键盘
    - (BOOL)textFieldShouldReturn:(UITextField *)textField
    {
        return [textField resignFirstResponder];
    }
    2、点击背景View收起键盘（你的View必须是继承于UIControl）
    
    [self.view endEditing:YES];
    3、你可以在任何地方加上这句话，可以用来统一收起键盘
    
    [[[UIApplication sharedApplication] keyWindow] endEditing:YES];

--- 04_关闭/收起键盘方法总结/ViewController.m
+++ 04_关闭/收起键盘方法总结/ViewController.m
@@ -17,11 +17,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    /**
+     * 2、点击背景View收起键盘（你的View必须是继承于UIControl）
+     */
+    [self.view endEditing:YES];
+    
+    /**
+     * 3、你可以在任何地方加上这句话，可以用来统一收起键盘
+     */
+    [[[UIApplication sharedApplication] keyWindow] endEditing:YES];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+/**
+ *  1、点击Return按扭时收起键盘
+ */
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    return [textField resignFirstResponder];
 }
 
 @end
