d123cb76f90c93f573dc3f06e5c7d65b9ed305d4
Yuen-rMBP <rxg9527@sina.cn>
2016-05-15 11:55:39 +0800

    0516 nil

---
+++ 02_获取 UIColor 的 RGB 值/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  02_获取 UIColor 的 RGB 值
+//
+//  Created by Yuen on 16/5/15.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ 02_获取 UIColor 的 RGB 值/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  02_获取 UIColor 的 RGB 值
+//
+//  Created by Yuen on 16/5/15.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
