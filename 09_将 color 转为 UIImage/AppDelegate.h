207ab3112d8981f10dc9e00d6cdd026f8fd50e61
Yuen-rMBP <rxg9527@sina.cn>
2016-05-17 00:09:30 +0800

    nil

---
+++ 09_将 color 转为 UIImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  09_将 color 转为 UIImage
+//
+//  Created by Yuen on 16/5/17.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 09_将 color 转为 UIImage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  09_将 color 转为 UIImage
+//
+//  Created by Yuen on 16/5/17.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
