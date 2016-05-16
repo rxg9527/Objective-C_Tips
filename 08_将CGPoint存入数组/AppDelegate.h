ebfa74d93d6f2fea19f6a91ada3b30b89262b285
Yuen-rMBP <rxg9527@sina.cn>
2016-05-16 15:42:34 +0800

    nil

---
+++ 08_将CGPoint存入数组/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  08_将CGPoint存入数组
+//
+//  Created by Yuen on 16/5/16.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
