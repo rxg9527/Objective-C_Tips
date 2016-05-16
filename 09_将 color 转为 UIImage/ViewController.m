1ce9e099dc15d42a3438d454741b021f1a368cbe
Yuen-rMBP <rxg9527@sina.cn>
2016-05-17 00:15:26 +0800

    将 color 转为 UIImage

--- 09_将 color 转为 UIImage/ViewController.m
+++ 09_将 color 转为 UIImage/ViewController.m
@@ -17,6 +17,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSUInteger R = (arc4random() % 256);
+    NSUInteger G = (arc4random() % 256);
+    NSUInteger B = (arc4random() % 256);
+    UIColor *randomColor = [UIColor colorWithRed:R/255.0 green:G/255.0 blue:B/255.0 alpha:1];
+    UIImage *image = [self createImageWithColor:randomColor];
+}
+
+- (UIImage *)createImageWithColor:(UIColor *)color
+{
+    CGRect rect = CGRectMake(0.0f, 0.0f, 1.0f, 1.0f);
+    UIGraphicsBeginImageContext(rect.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetFillColorWithColor(context, [color CGColor]);
+    CGContextFillRect(context, rect);
+    UIImage *theImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return theImage;
 }
 
 - (void)didReceiveMemoryWarning {
