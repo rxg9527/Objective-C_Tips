4d945c75f470face10dc729623bf390aa83a958c
Yuen-rMBP <rxg9527@sina.cn>
2016-05-16 15:45:01 +0800

    将CGPoint存入数组的两个方法

--- 08_将CGPoint存入数组/ViewController.m
+++ 08_将CGPoint存入数组/ViewController.m
@@ -17,6 +17,28 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    /**
+     *  方法1
+     */
+    CGPoint itemSprite1position = CGPointMake(100, 200);
+    NSMutableArray *array = [[NSMutableArray alloc] initWithObjects:NSStringFromCGPoint(itemSprite1position), nil];
+    //    从数组中取值的过程是这样的：
+    CGPoint point = CGPointFromString([array objectAtIndex:0]);
+
+    NSLog(@"point is %@.", NSStringFromCGPoint(point));
+    
+    /**
+     *  方法2
+     */
+    CGPoint itemSprite1position1 = CGPointMake(100, 200);
+    NSValue *originValue = [NSValue valueWithCGPoint:itemSprite1position1];
+    NSMutableArray *array1 = [[NSMutableArray alloc] initWithObjects:originValue, nil];
+    //    从数组中取值的过程是这样的：
+    NSValue *currentValue = [array1 objectAtIndex:0];
+    CGPoint point1 = [currentValue CGPointValue];
+
+    NSLog(@"point1 is %@.", NSStringFromCGPoint(point1));
 }
 
 - (void)didReceiveMemoryWarning {
