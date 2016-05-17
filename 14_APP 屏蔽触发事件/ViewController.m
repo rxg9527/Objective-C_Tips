80d5463fe7691ff62c360db0f7e038fd260805a3
Yuen-rMBP <rxg9527@sina.cn>
2016-05-18 00:39:05 +0800

    APP 屏蔽触发事件

--- 14_APP 屏蔽触发事件/ViewController.m
+++ 14_APP 屏蔽触发事件/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [[UIApplication sharedApplication] beginIgnoringInteractionEvents];
 }
 
 - (void)didReceiveMemoryWarning {
