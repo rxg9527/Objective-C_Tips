ab708634753f9fe59631dce4e0c65ffeb690b568
Yuen-rMBP <rxg9527@sina.cn>
2016-05-18 00:38:36 +0800

    nil

---
+++ 14_APP 屏蔽触发事件/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  14_APP 屏蔽触发事件
+//
+//  Created by Yuen on 16/5/18.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 14_APP 屏蔽触发事件/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  14_APP 屏蔽触发事件
+//
+//  Created by Yuen on 16/5/18.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
