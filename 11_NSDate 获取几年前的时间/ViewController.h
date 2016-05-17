f24418a1cfb24f54d8cdcf7397d8efa6d5413f2f
Yuen-rMBP <rxg9527@sina.cn>
2016-05-18 00:23:11 +0800

    nil

---
+++ 11_NSDate 获取几年前的时间/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  11_NSDate 获取几年前的时间
+//
+//  Created by Yuen on 16/5/18.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 11_NSDate 获取几年前的时间/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  11_NSDate 获取几年前的时间
+//
+//  Created by Yuen on 16/5/18.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
