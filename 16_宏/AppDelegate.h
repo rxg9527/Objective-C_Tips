42165458f965e908b85484a698ab634dc42a5d05
Yuen-rMBP <rxg9527@sina.cn>
2016-05-28 11:53:15 +0800

    nil

---
+++ 16_宏/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  16_宏
+//
+//  Created by Yuen on 16/5/28.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 16_宏/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  16_宏
+//
+//  Created by Yuen on 16/5/28.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
