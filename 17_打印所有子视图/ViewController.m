33bc204a12230915663247b6234a9ef1f7b03e9e
Yuen-iMac <rxg9527@sina.cn>
2016-05-31 11:05:45 +0800

    在控制台里输入 po [self.view recursiveDescription]      <UIView: 0x7fa0e9ce5190; frame = (0 0; 414 736); autoresize = W+H; layer = <CALayer: 0x7fa0e9ce2d70>>      | <_UILayoutGuide: 0x7fa0e9ce5560; frame = (0 0; 0 0); hidden = YES; layer = <CALayer: 0x7fa0e9ce3790>>      | <_UILayoutGuide: 0x7fa0e9ce60c0; frame = (0 0; 0 0); hidden = YES; layer = <CALayer: 0x7fa0e9ce2d10>>      | <UIView: 0x7fa0e9ce4800; frame = (0 0; 300 300); layer = <CALayer: 0x7fa0e9ce2510>>      | <UIView: 0x7fa0e9f73de0; frame = (0 300; 300 300); layer = <CALayer: 0x7fa0e9f47f00>>

--- 17_打印所有子视图/ViewController.m
+++ 17_打印所有子视图/ViewController.m
@@ -17,6 +17,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self.view addSubview: ({
+        UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 300, 300)];
+        view.backgroundColor = [UIColor redColor];
+        view;
+    })];
+    [self.view addSubview: ({
+        UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 300, 300, 300)];
+        view.backgroundColor = [UIColor blueColor];
+        view;
+    })];
+    /**
+     *  在控制台里输入 po [self.view recursiveDescription]
+     <UIView: 0x7fa0e9ce5190; frame = (0 0; 414 736); autoresize = W+H; layer = <CALayer: 0x7fa0e9ce2d70>>
+     | <_UILayoutGuide: 0x7fa0e9ce5560; frame = (0 0; 0 0); hidden = YES; layer = <CALayer: 0x7fa0e9ce3790>>
+     | <_UILayoutGuide: 0x7fa0e9ce60c0; frame = (0 0; 0 0); hidden = YES; layer = <CALayer: 0x7fa0e9ce2d10>>
+     | <UIView: 0x7fa0e9ce4800; frame = (0 0; 300 300); layer = <CALayer: 0x7fa0e9ce2510>>
+     | <UIView: 0x7fa0e9f73de0; frame = (0 300; 300 300); layer = <CALayer: 0x7fa0e9f47f00>>
+     */
 }
 
 - (void)didReceiveMemoryWarning {
