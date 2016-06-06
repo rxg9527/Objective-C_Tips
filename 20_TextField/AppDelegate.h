c7fdef95af2227851a33d53ca71ae833f07b2338
Yuen-iMac <rxg9527@sina.cn>
2016-06-06 16:02:51 +0800

    nil

---
+++ 20_TextField/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20_TextField
+//
+//  Created by Yuen-iMac on 16/6/6.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 20_TextField/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20_TextField
+//
+//  Created by Yuen-iMac on 16/6/6.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
