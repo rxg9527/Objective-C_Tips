e1a43a108063950ce5c6eec74813fbd286e2573b
Yuen-rMBP <rxg9527@sina.cn>
2016-06-22 11:43:03 +0800

    nil

---
+++ 22_手机联系人选择/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  22_手机联系人选择
+//
+//  Created by Yuen on 16/6/22.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ 22_手机联系人选择/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  22_手机联系人选择
+//
+//  Created by Yuen on 16/6/22.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
