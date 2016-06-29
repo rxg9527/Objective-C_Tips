17d440b1ddbfb554689108fc6544fd70016d5933
Yuen-rMBP <rxg9527@sina.cn>
2016-06-29 17:51:15 +0800

    阮晓刚 nil

---
+++ 24_Masonry动画/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  24_Masonry动画
+//
+//  Created by Yuen on 16/6/29.
+//  Copyright © 2016年 Yuen. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "Masonry.h"
+
+@interface ViewController ()
+@property (strong, nonatomic) UIView *aniView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
